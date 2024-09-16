a68772cfb690b2f74715713e36d4d51a24f05d26
A-pilip <areoayomide2008@gmail.com>
2024-09-16 17:49:42 +0100

    updating the script

--- script/contact.js
+++ script/contact.js
@@ -4,16 +4,20 @@ btn.addEventListener("click", (event) => {
   var inputedName = document.getElementById("name").value;
   var inputedMessage = document.getElementById("message").value;
 
+  input
+
   if (inputedName.length === 0) {
     event.preventDefault();
     document.getElementById("name-error").innerText =
       "Enter your name into this field";
   } else {
-    console.log(inputedName);
-    if (inputedName) {
-        
+    if (inputedName.indexOf(" ") < 0) {
+      event.preventDefault();
+      document.getElementById("name-error").innerText =
+        "You only Inputed your Surname or Lastname";
     } else {
-        
+      console.log("False");
+      console.log(inputedName.lastIndexOf(" "));
     }
   }
   event.preventDefault();
