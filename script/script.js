590005735833424517c2b122ac6cc57399f5004a
A-pilip <areoayomide2008@gmail.com>
2024-09-17 14:48:56 +0100

    updating style

--- script/script.js
+++ script/script.js
@@ -3,3 +3,7 @@ document.getElementById("date").innerText = date.getFullYear();
 
 var image = document.getElementsByTagName("img");
 console.log(image);
+
+for (let i = 0; i < image.length; i++) {
+  
+}
